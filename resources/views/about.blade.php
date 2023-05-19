eb8f6a5da89d8424497d3e83c54cc07927c7f316
Smackigan <pablovanduo@gmail.com>
2023-05-19 13:07:24 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout meta-title="Jean Paul blog - About us page" meta-description="Lorem ipsum">
+
+    <section class="w-full flex flex-col items-center px-3">
+    
+        <article class="flex flex-col shadow my-4">
+            <!-- Article Image -->
+            <a href="#" class="hover:opacity-75 w-full">
+                <img src="/storage/{{ $widget->image }}">
+            </a>
+            <div class="bg-white flex flex-col justify-start p-6">
+                <h1 class="text-3xl font-bold hover:text-gray-700 pb-4">
+                    {{ $widget->title }}
+                </h1>
+                <div>
+                    {!! $widget->content !!}
+                </div>
+            </div>
+        </article>
+
+    </section>
+
+</x-app-layout>
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,18 @@
+<?php
+/** @var $posts \Illuminate\Pagination\LengthAwarePaginator */
+?>
+
+<x-app-layout :meta-title="'The Jean Paul blog - posts by category ' . $category->title" meta-description="By category description">
+
+<section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+@foreach ($posts as $post)
+    <x-post-item :post="$post"></x-post-item>
+@endforeach
+
+    {{ $posts->onEachSide(1)->links() }}
+
+</section>
+
+    <x-sidebar />
+</x-app-layout>
